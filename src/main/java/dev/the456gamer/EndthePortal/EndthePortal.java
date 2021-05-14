50268956b51a36abb46b4e83ee2547d382c7e431
The456gamer <The456gamer@the456gamer.dev>
2021-05-14 23:22:27 +0100

    init repo

---
+++ src/main/java/dev/the456gamer/EndthePortal/EndthePortal.java
@@ -0,0 +1,17 @@
+package dev.the456gamer.EndthePortal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EndthePortal extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
